dcb92aa8c2e93986a9a5c6f987c001d990f6d6aa
WallaceAnterio <wallacesanterio@gmail.com>
2022-04-05 14:41:48 -0300

    creating-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { SuccessComponent } from './success/success.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    SuccessComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
