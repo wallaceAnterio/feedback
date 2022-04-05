7de9e71f9b979664f6fd0db69ec24ed611363d72
WallaceAnterio <wallacesanterio@gmail.com>
2022-04-05 14:46:28 -0300

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SuccessComponent } from './success/success.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+const routes: Routes = [
+  { path: '', component: FeedbackComponent },
+  { path: 'success', component: SuccessComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
