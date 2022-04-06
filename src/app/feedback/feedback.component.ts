133a3bb2abab1c25b4b02709ea2c1c4bc8c70764
WallaceAnterio <wallacesanterio@gmail.com>
2022-04-06 00:43:18 -0300

    creating reactive form

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.scss']
+  styleUrls: ['./feedback.component.scss'],
 })
 export class FeedbackComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  feedbackForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.email, Validators.required]],
+    type: ['', Validators.required],
+    description: ['', Validators.required],
+    rating: [0, Validators.min(1)],
+  });
+
+  erroMsg = '';
+
+  closeError() {
+    this.erroMsg = ''
+    console.log('clicou')
   }
 
+  onSubmit(){}
 }
